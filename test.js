173cc3810bea3c727d1c15fd9998327f9434178a
Hongtsewen <tsewen.hong@gmail.com>
2022-12-24 14:30:37 +0800

    modify

---
+++ test.js
@@ -0,0 +1,15 @@
+
+  // alert("Hello World!");
+  let temp;
+  temp = 50;
+  console.log(temp);
+
+  temp = 37;
+  console.log(temp);
+
+  let mondayTemp = 45;
+  let tuesdayTemp = mondayTemp;
+  console.log(tuesdayTemp);
+
+  const daysInweek = 7;
+  
